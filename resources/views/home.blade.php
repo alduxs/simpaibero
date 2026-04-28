a3e2eccbd74d3727735779f913857c4dd38d93fb
Aldo Iñiguez <ainiguez@cajaprevision.org>
2026-04-28 07:54:02 -0300

    espacios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,21 +24,16 @@
                             <div class="col-lg-5">
                                 <div class="text-center mt-5 mb-5">
 
-
                                     <h5 class="fs-lg mb-1">Simpa Iberoamericana</h5>
                                     <p class="text-muted fs-sm">
                                         Bienvenido al administrador de contenidos de la web.
                                     </p>
-
-
 
                                 </div>
                             </div>
                             <!-- end col -->
                         </div>
                         <!-- end row -->
-
-
                     </div>
                 </div>
             </div><!-- end card-body -->
