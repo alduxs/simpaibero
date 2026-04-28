8148e1adebfb74c67b69e6cc00f5e7a562c1def7
Aldo Iñiguez <ainiguez@cajaprevision.org>
2026-04-28 08:56:25 -0300

    cambios

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://simpaibero.com/public/assets/images/logo-black.png" class="logo" alt="Simpa Iberoamericana S.A.">
+<img src="https://simpaibero.com/public/assets/images/logo-black.png" class="logo" alt="Simpa Iberoamericana S.A." style="width: 142px; height: 50px; max-width: none;">
 @else
 {!! $slot !!}
 @endif
